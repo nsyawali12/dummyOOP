72d6b709c3cdf3ee7611bd2ab358348841e1122c
nsyawali12 <nsyawali12@gmail.com>
2020-07-03 17:36:09 +0700

    inheritance

--- py_oopbeginner3.py
+++ py_oopbeginner3.py
@@ -1,20 +1,33 @@
 class Pet:
-  def __init__ (self, name, age):
+  def __init__(self, name, age):
     self.name = name
     self.age = age
   
   def show(self):
     print(f"I am {self.name} and I am {self.age} years old")
+  
+  def speak(self):
+    print("I don't know what I say")
 
 class Cat(Pet):
+  def __init__(self, name, age, color):
+    super().__init__(name, age)
+    self.color = color
+
   def speak(self):
     print("Meow")
+  
+  def show(self):
+      print(f"I am {self.name} and I am {self.age} years old and I am {self.color}")
 
 class Dog(Pet):
   def speak(self):
     print("Bark")
 
-p = Pet("Tim", 19)
+
+p = Pet("panpan", 19)
 p.show()
-c = Cat("Bill", 34)
-c.show()
+c = Cat("raisa", 34)
+c.speak()
+
+### Learning basic inheritance
